fdbebd446214406abb0811851f61c584e7edcf78
BeautifulTimes <yidengqd@gmail.com>
2022-05-22 21:03:51 -0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -61,13 +61,11 @@ class register(Resource):
 api.add_resource(Users, '/users')  # '/users' is our entry point
 api.add_resource(login, '/login')  # '/users' is our entry point
 api.add_resource(register, '/register')  # '/users' is our entry point
-for(;;)
-{
+while 1==1:
     sleep(1000)
     f = open("myfile.txt","w")
     f.write("Hello \n")
     f.close() #to change file access modes
-}
 if __name__ == '__main__':
     print("running")
     app.run()  # run our Flask app
