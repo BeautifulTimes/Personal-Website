c32a5ada2420758b8dac1058467e7acb2f69380c
BeautifulTimes <yidengqd@gmail.com>
2022-06-01 19:40:09 -0400

    Update backend.py

--- backend.py
+++ backend.py
@@ -72,7 +72,7 @@ class Users(Resource):
         if args['username'] in userdata:
             if args['value'] not in scoredata[args['username']]:
                 scoredata[args['username']][args['value']] = 1
-                scoredata[args['user']]['time'] = time.time() 
+                scoredata[args['username']]['time'] = time.time() 
             else:
                 return 512
 
