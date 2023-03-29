3979b5a694335fe814baee7a04b956bdb77eedc6
root <yidengqd@gmail.com>
2023-03-29 21:48:34 +0000

    More chatbot stuff

--- backend.py
+++ backend.py
@@ -6,33 +6,36 @@ from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 import os
 import openai
-
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
-
+curuser = -1
+allconversations = []
 class feedback(Resource):
     def get(self):
-        return userdata , 200
+        global curuser
+        global allconversations
+        curuser = curuser + 1
+        allconversations.append([{"role": "system", "content": "You are Amy, a certified psychologist and therapist. Respond to me as amy."}])
+        print(curuser)
+        return curuser , 200
     def post(self):
-        parser = reqparse.RequestParser()  # initialize
+        parser = reqparse.RequestParser() 
         parser.add_argument('feedback')
-        args = parser.parse_args()  # parse arguments to dictionary       
+        parser.add_argument('userid')
+        args = parser.parse_args()     
         print(args['feedback'])
+        print(args['userid'])
+        userid =  int(args['userid'])
         openai.api_key = os.getenv("OPENAI_API_KEY")
-        response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="Create a 500 word Essay on the following target:\n" + args['feedback'],
-        temperature=0.7,
-        max_tokens=600,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
+        allconversations[userid].append({"role": "user", "content": args['feedback']})
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=  allconversations[userid]
         )
-        print(response);
-        print(type(response))
-        responsetext = response["choices"][0]["text"]
+        responsetext = response["choices"][0]["message"]["content"]
         responsetext = responsetext.strip()
+        allconversations[userid].append({"role": "assistant", "content": responsetext})
         return responsetext
     pass
 
