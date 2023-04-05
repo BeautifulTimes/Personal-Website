7520c62e9fddac07dfc6f51007e0f3c7266ec66b
root <yidengqd@gmail.com>
2023-04-05 03:38:07 +0000

    generation of voice response

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
 from flask import Flask
-import time
-import threading
 import ast
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
@@ -11,12 +11,33 @@ api = Api(app)
 CORS(app)
 curuser = -1
 allconversations = []
+numberofmessages = []
+session = Session(profile_name="default")
+polly = session.client("polly")
+def saysomething(input,counter):
+    try:
+        response = polly.synthesize_speech(Text=input, OutputFormat="mp3",
+                                        VoiceId="Joanna")
+    except (BotoCoreError, ClientError) as error:
+        print(error)
+        sys.exit(-1)
+
+    if "AudioStream" in response:
+        numberofmessages[counter] = numberofmessages[counter]+1
+        file = open(str(numberofmessages[counter]) + '_' + str(counter) + 'speech.mp3', 'wb')
+        file.write(response['AudioStream'].read())
+        file.close()
+    else:
+        # The response didn't contain audio data, exit gracefully
+        print("Could not stream audio")
+        sys.exit(-1)
 class feedback(Resource):
     def get(self):
         global curuser
         global allconversations
         curuser = curuser + 1
         allconversations.append([{"role": "system", "content": "You are Amy, a certified psychologist and therapist. Respond to me as amy."}])
+        numberofmessages.append(0)
         print(curuser)
         return curuser , 200
     def post(self):
@@ -27,14 +48,17 @@ class feedback(Resource):
         print(args['feedback'])
         print(args['userid'])
         userid =  int(args['userid'])
+        if(userid > curuser):
+            return "something went wrong"
         openai.api_key = os.getenv("OPENAI_API_KEY")
         allconversations[userid].append({"role": "user", "content": args['feedback']})
         response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=  allconversations[userid]
+            model="gpt-3.5-turbo",
+            messages=  allconversations[userid]
         )
         responsetext = response["choices"][0]["message"]["content"]
         responsetext = responsetext.strip()
+        saysomething(responsetext,userid)
         allconversations[userid].append({"role": "assistant", "content": responsetext})
         return responsetext
     pass
